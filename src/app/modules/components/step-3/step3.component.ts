020f8a29bd731041b25e22b9871ab1d1e0f47d7b
Diego Pagini <diegopaginidev@gmail.com>
2021-12-21 15:43:51 +0100

    lazy load steps

--- src/app/modules/components/step-3/step3.component.ts
+++ src/app/modules/components/step-3/step3.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatStepper } from '@angular/material/stepper';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { GameService } from '../../services/game.service';
 
@@ -11,7 +12,11 @@ import { GameService } from '../../services/game.service';
 export class Step3Component implements OnInit {
   score$: Observable<number>;
 
-  constructor(private gameService: GameService, private stepper: MatStepper) {}
+  constructor(
+    private gameService: GameService,
+    private stepper: MatStepper,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.score$ = this.gameService.getWins();
@@ -21,5 +26,7 @@ export class Step3Component implements OnInit {
     this.stepper.reset();
   }
 
-  handleNo(): void {}
+  handleNo(): void {
+    this.router.navigate(['/welcome']);
+  }
 }
